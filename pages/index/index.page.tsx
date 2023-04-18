9759722c4298da0b80b64f5e978fb3ce258f188e
Stuart Lynn <stuart.lynn@gmail.com>
2023-04-18 14:47:11 +0700

    adding adobe spectrum to show bug

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Counter } from './Counter'
+import { Heading, SSRProvider } from "@adobe/react-spectrum"
 
 export { Page }
 
 function Page() {
   return (
     <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
+      <SSRProvider>
+        <Heading> This is a heading</Heading>
+        <h1>Welcome</h1>
+        This page is:
+        <ul>
+          <li>Rendered to HTML.</li>
+          <li>
+            Interactive. <Counter />
+          </li>
+        </ul>
+      </SSRProvider>
     </>
   )
 }
